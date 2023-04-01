79ee93cc108c11cb802e1aa3d0b1676ac60f8f9f
Aberratio <joannakokot97@gmail.com>
2023-04-01 12:26:57 +0200

    initialize cypress

---
+++ cypress/e2e/formTests.cy.js
@@ -0,0 +1,26 @@
+describe("Choose Station Page", () => {
+  beforeEach(() => {
+    cy.visit("https://aberratio.github.io/choose-station/");
+  });
+
+  it("should display a list of stations", () => {
+    cy.get("#root_fuelType").click();
+    cy.get(".station-list li").should("have.length.greaterThan", 0);
+  });
+
+  it("should allow a station to be selected", () => {
+    cy.get(".station-list li").first().click();
+    cy.get(".selected-station").should("contain", "You selected");
+  });
+
+  it("should display an error if no station is selected", () => {
+    cy.get("button").contains("Continue").click();
+    cy.get(".error-message").should("contain", "Please select a station");
+  });
+
+  it("should allow the user to continue after selecting a station", () => {
+    cy.get(".station-list li").first().click();
+    cy.get("button").contains("Continue").click();
+    cy.url().should("include", "/details");
+  });
+});
